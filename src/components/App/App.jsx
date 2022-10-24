1d38c3391bac38439ecb4ec9fb3b0a38337a817e
Yurii <ivaschenko_u@ukr.net>
2022-10-24 23:39:24 +0300

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Route, Routes, Link} from 'react-router-dom'
+import Main from '../Main/Main'
+import Projects from '../Projects/Projects'
+import Skils from '../Skils/Skils'
+import s from './App.module.css'
+
+const App = () => {
+  return (
+   < >
+   <div className={s.app}>
+   <Main/>
+   <div >
+  <header>
+<Link to="/skils">Skils</Link>
+<Link to="/projects">Projects</Link>
+  </header>
+  
+
+<Routes>
+<Route path="/skils" element={<Skils/>}/>
+<Route path="/projects" element={<Projects/>}/>
+</Routes>
+</div>
+</div>
+</>
+  )
+}
+
+export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './Main.module.css'
+
+const Main = () => {
+  return (
+    <div className={s.main}>Main</div>
+  )
+}
+
+export default Main
