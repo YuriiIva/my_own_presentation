c6af149c90cab21705f81ed84fbd0b5007f3cfb0
Yurii <ivaschenko_u@ukr.net>
2022-11-14 09:09:14 +0200

    add some projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import s from "./Progects.module.css"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+   
+      <ul className={s.container}>
+        <li className={s.prog_item}><img src={require("../../images/PRO-Test.png" )} alt="" className={s.img}/></li>
+        <li className={s.prog_item}><img src={require("../../images/Kapusta.png" )} alt="" className={s.img}/></li>
+        <li className={s.prog_item}><img src={require("../../images/Cinema-library.png" )} alt="" className={s.img}/></li>
+        <li className={s.prog_item}><img src={require("../../images/Game-Snake.png" )} alt="" className={s.img}/></li>
+        <li className={s.prog_item}><img src={require("../../images/PhoneBook.png" )} alt="" className={s.img}/></li>
+        <li className={s.prog_item}><img src={require("../../images/find_images.png" )} alt="" className={s.img}/></li>
+      </ul>
+   
   )
 }
 
