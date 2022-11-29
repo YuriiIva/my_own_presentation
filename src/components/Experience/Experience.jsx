a27a8da31f94ac8e0a119c382eb00649f42b91e4
Yurii <ivaschenko_u@ukr.net>
2022-11-29 22:30:08 +0200

    start exp

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+import s from "./Experience.module.css"
+
 const Experience = () => {
+
+
+
   return (
-    <div>Experience</div>
+    <div className={s.exper}>
+      <p>
+        Hello!
+        I didn't work, but..
+        I have some my own projects!
+        There some of them
+        </p>
+      <h2>My experience</h2>
+      <ul>
+        <li>
+        
+           <a href="https://github.com/YuriiIva/snake-game-2" target="_blank" rel="noreferre" className={s.link}>
+           <img src={require("../../images/Game-Snake.png" )} alt="" className={s.img}/>
+            </a>
+
+        </li>
+        <li>
+        
+        <a href="https://github.com/YuriiIva/snake-game-2" target="_blank" rel="noreferre" className={s.link}>
+        <img src={require("../../images/PhoneBook.png" )} alt="" className={s.img}/>
+         </a>
+         
+     </li>
+        </ul>
+      
+    </div>
   )
 }
 
